29fc7640ac5e01c6d474ba88e7c1fe4a50ad9c55
Kelvin Chappell <kelvin.chappell@theguardian.com>
2014-07-14 14:45:19 +0100

    First attempt at NameExtractor

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,11 +1,13 @@
 package controllers
 
+import model.NameExtractor
 import play.api._
 import play.api.mvc._
 
 object Application extends Controller {
 
   def index = Action {
+    NameExtractor("abc Kelvin def Kelvin Chappell jjlkj") foreach println
     Ok(views.html.index("Your new application is ready."))
   }
 
