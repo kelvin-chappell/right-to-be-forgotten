8c926e340e9618c57508adeee0c04353be6713f5
Kelvin Chappell <kelvin.chappell@theguardian.com>
2014-07-14 15:03:38 +0100

    Testing NameExtractor

--- app/model/NameExtractor.scala
+++ app/model/NameExtractor.scala
@@ -3,7 +3,7 @@ package model
 object NameExtractor {
 
   def apply(body: String): Seq[String] = {
-    val regex = """\b[A-Z][a-z]+\s+\b[A-Z][a-z]+""".r
+    val regex = """\b[A-Z][a-z]+\s+\b[A-Z][A-Za-z]+""".r
 
     regex.findAllIn(body).toSeq
   }
