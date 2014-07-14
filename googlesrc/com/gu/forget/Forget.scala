eb97bbd046d766db1373eb3cf483d22331bc9aea
John Duffell <john.duffell@guardian.co.uk>
2014-07-14 14:10:27 +0100

    add more tidyup for forget checker

--- googlesrc/com/gu/forget/Forget.scala
+++ googlesrc/com/gu/forget/Forget.scala
@@ -6,13 +6,15 @@ import org.scalatest.selenium.WebBrowser
 
 /**
  * Created by jduffell on 14/07/2014.
+ *
+ * Just call isBlocked
  */
 class Forget extends WebBrowser {
 
   implicit val webDriver: WebDriver = new ChromeDriver
 
 //  val host = "https://www.google.co.uk/search?btnG=1&gws_rd=ssl&q="
-  val host = "file:///Users/jduffell/ws/right-to-be-forgotten/googlesrc/cheeseSearch.html"
+  val host = "file:///Users/jduffell/ws/right-to-be-forgotten/googlesrc/cheeseSearch.html?q="
 
   private def getUrlFromResult(element: Element) = {
     element.attribute("data-href").orElse(element.attribute("href")).get
@@ -23,7 +25,7 @@ class Forget extends WebBrowser {
     !results.contains(articleUrl)
   }
 
-  def getResults(terms: String): List[String] = {
+  private def getResults(terms: String): List[String] = {
     go to (host+terms.replaceAll(" ", "+") )
     println(s"title: $pageTitle")
     val results = findAll(xpath("//li[@class='g']//h3[@class='r']/a")).toList
@@ -43,8 +45,8 @@ object Forget {
 
   def main(args: Array[String]) {
     val forget = new Forget()
-    val cheeseBlocked = forget.isBlocked("", "http://www.cheese.com/")
-    val baddieBlocked = forget.isBlocked("", "http://www.baddie.com/")
+    val cheeseBlocked = forget.isBlocked("cheese", "http://www.cheese.com/")
+    val baddieBlocked = forget.isBlocked("cheese", "http://www.baddie.com/")
     forget.quit
     println(s"cheeseBlocked $cheeseBlocked")
     println(s"baddieBlocked $baddieBlocked")
