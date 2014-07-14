3835d3eff9d460ebbcb01a640f6871ef7b8ab15c
John Duffell <john.duffell@guardian.co.uk>
2014-07-14 14:01:26 +0100

    can now scrape the urls from a google page

--- googlesrc/com/gu/forget/Forget.scala
+++ googlesrc/com/gu/forget/Forget.scala
@@ -11,11 +11,20 @@ class Forget extends WebBrowser {
 
   implicit val webDriver: WebDriver = new ChromeDriver
 
-  val host = "http://www.google.co.uk"
+//  val host = "https://www.google.co.uk/search?btnG=1&gws_rd=ssl&q="
+  val host = "file:///Users/jduffell/ws/right-to-be-forgotten/googlesrc/cheeseSearch.html"
 
-  def isBlocked: Boolean = {
-    go to (host )
+  private def getUrlFromResult(element: Element) = {
+    element.attribute("data-href").orElse(element.attribute("href"))
+  }
+
+  def isBlocked(terms: String): Boolean = {
+    go to (host+terms.replaceAll(" ", "+") )
     println(s"title: $pageTitle")
+    val results = findAll(xpath("//li[@class='g']//h3[@class='r']/a")).toList
+    println("length: " + results.length)
+    val urls = results.map(getUrlFromResult)
+    println("urls: " + urls)
     false
   }
 
@@ -29,7 +38,7 @@ object Forget {
 
   def main(args: Array[String]) {
     val forget = new Forget()
-      forget.isBlocked
+      forget.isBlocked("")
     forget.quit
   }
 
