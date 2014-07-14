cddb8103e897c9acd1881c3edf279a32394c2411
Kelvin Chappell <kelvin.chappell@theguardian.com>
2014-07-14 16:34:44 +0100

    NameExtractor v2

--- app/model/NameExtractor.scala
+++ app/model/NameExtractor.scala
@@ -1,10 +1,28 @@
 package model
 
+import java.io.FileInputStream
+
+import controllers.Application
+import opennlp.tools.namefind.{NameFinderME, TokenNameFinderModel}
+
 object NameExtractor {
 
-  def apply(body: String): Seq[String] = {
-    val regex = """\b[A-Z][a-z]+\s+\b[A-Z][A-Za-z]+""".r
+  lazy val nameFinder = {
+    val modelIn = new FileInputStream("conf/en-ner-person.bin")
+    val model = new TokenNameFinderModel(modelIn)
+    val nameFinder = new NameFinderME(model)
+    modelIn.close()
+    nameFinder
+  }
 
-    regex.findAllIn(body).toSeq
+  def apply(body: String): Seq[String] = {
+    val text = Application.testBody.split("[^\\w]")
+
+    val nameSpans = nameFinder.find(text)
+
+    nameSpans map { span =>
+      val slice = text.slice(span.getStart, span.getEnd)
+      slice.mkString(" ")
+    }
   }
 }
--- build.sbt
+++ build.sbt
@@ -5,11 +5,10 @@ version := "1.0-SNAPSHOT"
 libraryDependencies ++= Seq(
   jdbc,
   anorm,
-  cache
-)     
+  cache,
+  "org.scalatest" %% "scalatest" % "2.1.4",
+  "org.seleniumhq.selenium" % "selenium-java" % "2.41.0",
+  "org.apache.opennlp" % "opennlp-tools" % "1.5.3"
+)
 
 play.Project.playScalaSettings
-
-libraryDependencies += "org.scalatest" %% "scalatest" % "2.1.4"
-
-libraryDependencies += "org.seleniumhq.selenium" % "selenium-java" % "2.41.0"
